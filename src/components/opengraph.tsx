81779ee967e872044755185cb84d3089259752ee
Angelo <angelo@angeloanan.xyz>
2020-11-09 13:08:06 +0700

    fix(opengraph): adapted from old website
    - Uses NextJS' router to solve OpenGraph URL
    - Changed the current image to /pfp.png

--- src/components/opengraph.tsx
+++ src/components/opengraph.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 
 interface OpenGraphProps {
   title?: string
@@ -10,12 +11,14 @@ interface OpenGraphProps {
 }
 
 const OpenGraph: React.FC<OpenGraphProps> = (props: OpenGraphProps) => {
+  const currentPath = useRouter().pathname
+
   return (
     <Head>
       <meta property="og:title" content={props.title != null ? `${props.title} | Christopher Angelo's Website` : 'Christopher Angelo\'s Website'} />
       <meta property="og:type" content={props.type ?? 'website'} />
-      <meta property="og:url" content={props.url ?? 'https://angeloanan.xyz'} />
-      <meta property="og:image" content={props.img ?? '/img/logo'} />
+      <meta property="og:url" content={'https://angeloanan.xyz' + currentPath} />
+      <meta property="og:image" content={props.img ?? '/pfp.png'} />
     </Head>
   )
 }
