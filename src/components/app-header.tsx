3a351dd2435e300fb8c239811d980ed57cc0e4a0
Angelo <angelo@angeloanan.xyz>
2020-11-11 00:25:47 +0700

    feat: favicon

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -10,6 +10,10 @@ const AppHeader: React.FC<AppHeaderProps> = (props) => {
   return (
     <Head>
       <title>{props.title != null ? `${props.title} | Christopher Angelo's Website` : 'Christopher Angelo\'s Website'}</title>
+      <link rel="apple-touch-icon" sizes="180x180" href="img/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="img/favicon-16x16.png" />
+      <link rel="manifest" href="img/site.webmanifest"/>
     </Head>
   )
 }
