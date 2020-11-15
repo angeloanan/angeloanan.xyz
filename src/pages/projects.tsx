7b55d7ff2e033395eca6d434ffbf8afab46bb417
Angelo <angelo@angeloanan.xyz>
2020-11-15 22:35:51 +0700

    feat(project-page): temporarily uses project card

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,12 +1,25 @@
 import * as React from 'react'
 
-import { Heading } from '@chakra-ui/react'
+import { Text, VStack, Wrap } from '@chakra-ui/react'
 import Navbar from '../components/navbar'
+import ContentSpacer from '../components/ui/content-spacer'
+import ProjectCard from '../components/ui/content-spacer/project-section/project-card'
 
 const ProjectsPage: React.FC = () => {
   return <>
     <Navbar />
-    <Heading>Projects Page</Heading>
+    <ContentSpacer p={4}>
+      <VStack spacing={4}>
+        <Text size="lg" fontWeight={600} p={4} alignSelf="flex-start">Here are some projects that I've been working on:</Text>
+
+        <Wrap spacing={4} justify="center">
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+        </Wrap>
+      </VStack>
+    </ContentSpacer>
   </>
 }
 
