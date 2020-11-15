ba26d340123588163820c55c23643ea6a594f46b
Angelo <angelo@angeloanan.xyz>
2020-11-15 22:32:36 +0700

    refactor(navbar): added about page
    - Uses my name to go back to the homepage
    - Added an about page

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -3,17 +3,20 @@ import * as React from 'react'
 import { Box, Flex, Heading, Spacer } from '@chakra-ui/react'
 
 import NavbarButton from './navbar-button'
+import Link from 'next/link'
 
 const Navbar: React.FC = () => {
   return (
     <>
       <Flex as="nav" maxW="6xl" mx="auto" px={4}>
-        <Box py={4}>
-          <Heading size="md">Angelo</Heading>
-        </Box>
+        <Link href="/" passHref>
+          <Box p={4} as="button">
+            <Heading size="md">Angelo</Heading>
+          </Box>
+        </Link>
         <Spacer/>
         <Flex>
-          <NavbarButton title="Home" link="/"/>
+          <NavbarButton title="About" link="/about"/>
           <NavbarButton title="Projects" link="/projects"/>
           <NavbarButton title="Contact" link="/contact"/>
         </Flex>
