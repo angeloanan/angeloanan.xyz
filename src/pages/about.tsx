2352e530c925e11e63cc05476f987ea26c37559a
Angelo <angelo@angeloanan.xyz>
2020-11-15 22:33:09 +0700

    feat: about page

---
+++ src/pages/about.tsx
@@ -0,0 +1,14 @@
+import { Heading } from '@chakra-ui/react'
+import * as React from 'react'
+import Navbar from '../components/navbar'
+
+const AboutPage: React.FC = () => {
+  return (
+    <>
+      <Navbar />
+      <Heading>Hello</Heading>
+    </>
+  )
+}
+
+export default AboutPage
