ee36a0eb71e29a024fbecb7d24f6b2401759daba
Angelo <angelo@angeloanan.xyz>
2020-11-16 11:38:26 +0700

    refactor(next-seo-config): add theme-color and shorten name

--- src/next-seo.config.ts
+++ src/next-seo.config.ts
@@ -2,14 +2,18 @@ import type { NextSeoProps } from 'next-seo'
 
 const NextSEOConfig: NextSeoProps = {
   title: 'Christopher Angelo\'s Website',
-  description: 'Hello, I\'m Christopher Angelo. This is my website which includes my projects, portfolio and more awesome stuff!',
+  description: 'Hello, I\'m Angelo. This is my website which includes my projects, portfolio and more awesome stuff!',
   canonical: 'https://angeloanan.xyz',
+  additionalMetaTags: [{
+    name: 'theme-color',
+    content: '#2234ce'
+  }],
   openGraph: {
     type: 'website',
     locale: 'en_US',
     url: 'https://www.angeloanan.xyz/',
     title: "Christopher Angelo's Website",
-    description: 'Hello, I\'m Christopher Angelo. This is my website which includes my projects, portfolio and more awesome stuff!',
+    description: 'Hello, I\'m Angelo. This is my website which includes my projects, portfolio and more awesome stuff!',
     site_name: 'Personal Website',
     images: [{
       url: '/pfp.png',
