b47c4b5e9bf0a42126607962e30d8b547dd87091
Christopher Angelo <angelo@angeloanan.xyz>
2021-01-02 10:20:35 +0700

    feat: app footer

---
+++ src/components/app-footer.tsx
@@ -0,0 +1,53 @@
+import * as React from 'react'
+
+import { FiGithub, FiLinkedin, FiMail, FiTwitter } from 'react-icons/fi'
+import { HStack, Icon, Link, VisuallyHidden } from '@chakra-ui/react'
+
+import NextLink from 'next/link'
+import footerLinks from '../_data/footerLinks.json'
+
+const SocialMediaSection: React.FC = () => {
+  return (
+    <HStack mb={4} spacing={4} justify='center'>
+      <NextLink href="https://github.com/angeloanan" passHref>
+        <a>
+          <VisuallyHidden>GitHub</VisuallyHidden>
+          <Icon as={FiGithub} opacity={0.7} w={5} h={5}/>
+        </a>
+      </NextLink>
+      <NextLink href="https://twitter.com/UwUngelo" passHref>
+        <a>
+          <VisuallyHidden>Twitter</VisuallyHidden>
+          <Icon as={FiTwitter} opacity={0.7} w={5} h={5}/>
+        </a>
+      </NextLink>
+      <NextLink href="https://linkedin.com/in/angeloanan" passHref>
+        <a>
+          <VisuallyHidden>Linkedin</VisuallyHidden>
+          <Icon as={FiLinkedin} opacity={0.7} w={5} h={5}/>
+        </a>
+      </NextLink>
+      <NextLink href="mailto:angelo@angeloanan.xyz" passHref>
+        <a>
+          <VisuallyHidden>Email</VisuallyHidden>
+          <Icon as={FiMail} opacity={0.7} w={5} h={5}/>
+        </a>
+      </NextLink>
+    </HStack>
+  )
+}
+
+export const AppFooter: React.FC = () => {
+  return (
+    <>
+      <SocialMediaSection />
+      <HStack mb={8} spacing={4} justify='center'>
+        {footerLinks.map(link => (
+          <NextLink href={link} passHref>
+            <Link fontSize='sm'opacity={0.7}>{link}</Link>
+          </NextLink>
+        ))}
+      </HStack>
+    </>
+  )
+}
