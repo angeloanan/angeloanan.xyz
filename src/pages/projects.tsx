ab1ebdd610b1a03a5e19730cd725557c17264788
Christopher Angelo <angelo@angeloanan.xyz>
2021-01-04 16:04:46 +0700

    fix(projects): search engine optimization and a11y
    * Added a proper page title
    * Uses semantic HTML

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -4,16 +4,20 @@ import { TextHeading, TextParagraph } from '../components/typography'
 
 import { Box } from '@chakra-ui/react'
 import ContentSpacer from '../components/ui/content-spacer'
+import { NextSeo } from 'next-seo'
 
 const ProjectsPage: React.FC = () => {
   return (
     <>
+      <NextSeo title='Projects' />
       <ContentSpacer>
-        <Box mb={4}>
-          <TextHeading as='h1'>Projects</TextHeading>
-          <TextParagraph>
-            Here are every project that I've worked for in the past
-          </TextParagraph>
+        <Box mb={4} as='article'>
+          <Box as='header'>
+            <TextHeading as='h1'>Projects I've worked on</TextHeading>
+            <TextParagraph>
+              Here are every project that I've worked for in the past
+            </TextParagraph>
+          </Box>
         </Box>
 
         <TextParagraph as='em'>
