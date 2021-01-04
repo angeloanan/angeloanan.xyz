38ae684db98169835bc6da6c50cc3bffd06238c6
Christopher Angelo <angelo@angeloanan.xyz>
2021-01-04 16:03:19 +0700

    fix(contact): search engine optimization and a11y
    * Uses semantic html
    * Added proper page title
    * Renamed component

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -2,17 +2,26 @@ import * as React from 'react'
 
 import { TextHeading, TextParagraph } from '../components/typography'
 
+import { Box } from '@chakra-ui/react'
 import ContentSpacer from '../components/ui/content-spacer'
+import { NextSeo } from 'next-seo'
 
-const HomePage: React.FC = () => {
+const ContactsPage: React.FC = () => {
   return (
-    <ContentSpacer>
-      <TextHeading>Contact Me</TextHeading>
-      <TextParagraph>
-        Whew, wanna talk about something with me? Let's talk!
-      </TextParagraph>
-    </ContentSpacer>
+    <>
+      <NextSeo title='Contacts' />
+      <ContentSpacer>
+        <Box as='article'>
+          <Box as='header'>
+            <TextHeading as='h1'>Contact Me</TextHeading>
+            <TextParagraph>
+              Whew, wanna talk about something with me? Let's talk!
+            </TextParagraph>
+          </Box>
+        </Box>
+      </ContentSpacer>
+    </>
   )
 }
 
-export default HomePage
+export default ContactsPage
