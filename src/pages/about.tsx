8662251f27d66d9ca89b143a347bf4a39d622750
Christopher Angelo <angelo@angeloanan.xyz>
2021-01-04 16:07:14 +0700

    refactor(about): extract nextseo to parent element

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -7,19 +7,21 @@ import { NextSeo } from 'next-seo'
 
 const AboutPage: React.FC = () => {
   return (
-    <ContentSpacer>
+    <>
       <NextSeo title='About Me' />
-      <TextHeading as='h1'>About Me</TextHeading>
-      <TextParagraph>
-        Hi, I'm Christopher Angelo - A full-stack web developer based on
-        Jakarta, Indonesia. I've started to learn how to code out of curiosity
-        when I was 13 years old.
-      </TextParagraph>
+      <ContentSpacer>
+        <TextHeading as='h1'>About Me</TextHeading>
+        <TextParagraph>
+          Hi, I'm Christopher Angelo - A full-stack web developer based on
+          Jakarta, Indonesia. I've started to learn how to code out of curiosity
+          when I was 13 years old.
+        </TextParagraph>
 
-      <TextParagraph as='em'>
-        This page is still work in progress...
-      </TextParagraph>
-    </ContentSpacer>
+        <TextParagraph as='em'>
+          This page is still work in progress...
+        </TextParagraph>
+      </ContentSpacer>
+    </>
   )
 }
 
