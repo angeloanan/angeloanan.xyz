54216109ae36b87ba173c1911cf2c855fcd19d53
Christopher Angelo <angelo@angeloanan.xyz>
2021-01-25 23:41:18 +0700

    security: include everything else in CSP

--- next.config.js
+++ next.config.js
@@ -29,7 +29,8 @@ module.exports = {
           },
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'"
+            value:
+              "default-src 'none'; script-src 'self'; connect-src 'self'; img-src 'self'; style-src 'self';base-uri 'self';form-action 'self'"
           },
           {
             key: 'Strict-Transport-Security',
