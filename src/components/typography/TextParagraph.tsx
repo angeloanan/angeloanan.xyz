88fbd5fcce8c74e8503337ff67c359bcba4336e0
Christopher Angelo <angelo@angeloanan.xyz>
2021-03-03 23:46:27 +0700

    refactor(textparagraph): uses absolute color rather than opacity

--- src/components/typography/TextParagraph.tsx
+++ src/components/typography/TextParagraph.tsx
@@ -3,7 +3,5 @@ import * as React from 'react'
 import { Text, TextProps } from '@chakra-ui/react'
 
 export const TextParagraph: React.FC<TextProps> = props => {
-  return (
-    <Text color='gray' opacity={0.7} letterSpacing={0.2} as='p' {...props} />
-  )
+  return <Text color='gray.200' letterSpacing={0.2} as='p' {...props} />
 }
