cfc7528c2d2d044856dad60b5e8452c1bfce8484
Christopher Angelo <angelo@angeloanan.xyz>
2022-03-10 01:23:24 +0700

    feat(404): spacing

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -6,7 +6,7 @@ import BlahajImage from '../../public/images/404.png'
 
 const NotFoundPage: NextPage = () => {
   return (
-    <main className='flex w-full flex-col items-center justify-center'>
+    <main className='flex w-full flex-col items-center justify-center px-4'>
       <div className='mt-16 w-48 sm:w-72'>
         <Image src={BlahajImage} alt='' className='rounded-3xl shadow-md' placeholder='blur' />
       </div>
@@ -19,7 +19,8 @@ const NotFoundPage: NextPage = () => {
       <p className='mt-4 text-center text-lg font-semibold tracking-tight text-neutral-600'>
         The page you were looking for doesn&apos;t exist or was moved.
       </p>
-      <p className='mt-2 text-lg font-light tracking-tight text-neutral-500'>Blåhaj is angry : (</p>
+
+      <p className='mt-8 text-lg font-light tracking-tight text-neutral-500'>Blåhaj is angry : (</p>
     </main>
   )
 }
