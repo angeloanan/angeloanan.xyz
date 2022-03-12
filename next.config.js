527f4f3e92e86550bb43b3c569fdb31f2eded269
Christopher Angelo <angelo@angeloanan.xyz>
2022-03-12 07:58:25 +0700

    feat: enable avif

--- next.config.js
+++ next.config.js
@@ -13,6 +13,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   optimizeFonts: true,
+  images: {
+    formats: ['image/avif', 'image/webp']
+  },
   i18n: {
     locales: ['en-US'],
     defaultLocale: 'en-US'
